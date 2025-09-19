96143902ec620c39ddd71ee0691fd44f10b67463
Aaron349899401 <aarongoogoo292gmail.com>
2025-09-19 10:56:25 -0400

    among us

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,13 @@
 # Lesson 4
+from math import ceil
+
+fence1 = len(str(input()))
+fence2 = len(str(input()))
+fence3 = len(str(input()))
+
+Total = fence1 + fence2 + fence3
+prime = ceil(Total / 12)
+
+print(prime)
+print(Total % 12)
+print(Total * 14.95)
