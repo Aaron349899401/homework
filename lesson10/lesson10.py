064abc2993bf2f9c222ebae76d93d29ae192234f
unknown <aarongoogoo29@gmail.com>
2025-09-21 12:56:33 -0400

    completed

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,13 @@
 # Lesson 10
+# input always returns a string by defult
+digits = input("what are the last four digits of your number?: ")
+if digits[0] in ["8", "9"]:
+    if digits[1] == digits[2]:
+        if digits[3] in ["8", "9"]:
+            print("Telemarketer")
+        else:
+            print("Not a Telemarketer")
+    else:
+        print("Not a Telemarketer")
+else:
+    print("Not a Telemarketer")
