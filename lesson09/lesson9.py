435546b99c91114270a0a1cf6f35997f0fa971e3
unknown <aarongoogoo29@gmail.com>
2025-09-21 12:37:05 -0400

    completed

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,32 @@
 # Lesson 9
+import random
+# This part is here to recieve input from the player a
+# Also to make sure they only inputted either rock, paper, or scissors
+amongus = True
+
+while amongus:
+    player = input("What did you choose? (rock, paper, or scissors): ")
+    if player in {"rock", "paper", "scissors"}:
+        amongus = False
+# Something to compare the players result to (the opponent)
+CPU = random.choice(["rock", "paper", "scissors"])
+print(f"The CPU chose {CPU}")
+# Whether or not the player won
+if player == CPU:
+    print("DRAW")
+elif player == "rock":
+    if CPU == "scissors":
+        print("YOU WIN!")
+    else:
+        print("YOU LOSE!")
+elif player == "paper":
+    if CPU == "rock":
+        print("YOU WIN!")
+    else:
+        print("YOU LOSE!")
+elif player == "scissors":
+    if CPU == "paper":
+        print("YOU WIN!")
+    else:
+        print("YOU LOSE!")
+# End of game
