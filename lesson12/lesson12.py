98ce0a21f630c778df2faf43032c57f32cd6d231
unknown <aarongoogoo29@gmail.com>
2025-09-23 09:47:39 -0400

    completed

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,25 @@
 # Lesson 12
+a = int(input("Enter the first angle: "))
+b = int(input("Enter the second angle: "))
+c = int(input("Enter the third angle: "))
+while True:
+    abc = [a, b, c]
+    if sum(abc) == 180:
+        if a == b == c:
+            print("You have an Equilateral Triangle!")
+        elif a != b or a != c:
+            if b == c:
+                print("You have an Isosceles Triangle!")
+            elif b != c:
+                print("You have a Scalene Triangle!")
+        elif b != a or b != c:
+            if a == c:
+                print("You have an Isosceles Triangle!")
+            elif a != c:
+                print("You have a Scalene Triangle!")
+        elif c != a or c != b:
+            if a == b:
+                print("You have an Isosceles Triangle!")
+            elif a != b:
+                print("You have a Scalene Triangle!")
+    break
