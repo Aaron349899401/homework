03211db8ba0bca5a538a2a7214588f540d0473b6
unknown <aarongoogoo29@gmail.com>
2025-09-24 11:46:08 -0400

    completed

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,17 @@
 # Lesson 13
+month = [1, 12]
+day = [1, 31]
+month = int(input("Enter the month: "))
+day = int(input("Enter the day: "))
+
+if month == 2 and day == 18:
+    print("Special")
+elif month > 2:
+    print("After")
+elif month < 2:
+    print("Before")
+elif month == 2:
+    if day > 18:
+        print("After")
+    elif day < 18:
+        print("Before")
