1d07828ec5632192a70a20aad4ec39dc2e0a30dd
unknown <aarongoogoo29@gmail.com>
2025-09-25 12:59:38 -0400

    completed

--- lesson17/lesson17.py
+++ lesson17/lesson17.py
@@ -1,1 +1,8 @@
 # Lesson 17
+num = int(input("Enter a number: "))
+factorial = num
+yo = 1
+while factorial > 1:
+    yo = factorial * butt
+    factorial -= 1
+print(f"The factorial of {num} is {yo}")
