50a94b75a32fc4e5d32e3d9a1bdbbc7bea2f8410
unknown <aarongoogoo29@gmail.com>
2025-10-01 09:31:07 -0400

    completed

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,14 @@
 # Lesson 22
+Nterm = int(input("Enter a number: "))
+t1 = 0
+t2 = 1
+if Nterm == 1:
+    tN = t2
+elif Nterm == 0:
+    tN = t1
+for i in range(3, Nterm + 1):
+    tN = t1 + t2
+    t1 = t2
+    t2 = tN
+
+print(f"Term number {Nterm} has a value of {tN} in the fibonacci sequence")
