ca34f9e7323bd8b157b0b327709e870845d093b9
unknown <aarongoogoo29@gmail.com>
2025-09-30 20:30:37 -0400

    complete

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,16 @@
 # Lesson 19
+num = int(input("Enter a number: "))
+count = True
+if count > 2:
+    count = False
+else:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            count = False
+            break
+
+if count:
+    print("PRIME!")
+else:
+    print("Not PRIME!")
+     
