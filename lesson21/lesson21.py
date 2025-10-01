25f7aecb6944f451f42401fb9237b445be07a04e
unknown <aarongoogoo29@gmail.com>
2025-09-30 21:23:50 -0400

    completed

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,15 @@
 # Lesson 21
+max = 0
+num_max = 1
+
+N = int(input("Enter a number: "))
+for num in range(1, N + 1):
+    count = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            count += 1
+    if count > max:
+        max = count
+        num_max = num
+
+print(f"{num_max} has the most factors within the range of 1 to {N}")
