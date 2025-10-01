11d1f905f9d5e8c3d9108014099ea4c91205311d
unknown <aarongoogoo29@gmail.com>
2025-10-01 11:50:25 -0400

    completed

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,17 @@
 # Lesson 23
+ching = True
+count = 0
+total_sum = 0
+while ching:
+    num = input("Enter you numbers: ")
+    if num.lower().capitalize() == "Exit":
+        ching = False
+        break
+    else:
+        chong = int(num)
+        count += 1
+        total_sum += chong
+
+dong = total_sum / count
+print(f"The average is {dong}!")
+
