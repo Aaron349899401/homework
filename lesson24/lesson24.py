29824eccd9ef0edd947c05f1d74fbfdc83efbb84
unknown <aarongoogoo29@gmail.com>
2025-10-02 10:08:24 -0400

    completed

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,14 @@
 # Lesson 24
+sigma = True
+length = []
+while sigma:
+    noway = input("Enter your word: ")
+    if noway.capitalize() == "X":
+        sigma = False
+        break
+    else:
+        length.append(noway)
+
+ofcourse = max(length, key=len)
+
+print(f"The longest word is {ofcourse}")
