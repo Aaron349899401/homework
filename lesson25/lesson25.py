ce463684da899d5f6bacbb960e63b572f35f753a
unknown <aarongoogoo29@gmail.com>
2025-10-04 17:11:00 -0400

    completed

--- lesson25/lesson25.py
+++ lesson25/lesson25.py
@@ -1,1 +1,21 @@
 # Lesson 25
+numb = int(input("Enter a number: "))
+
+def prime_factors(numb):
+    largest = 0
+    for wow in range(1, int(numb // 2) + 1):
+        if numb % wow == 0:
+            prime = True
+            for i in range(2, int(wow ** 0.5) + 1):
+                if wow % i == 0:
+                    prime = False
+                    break
+            if prime:
+                largest = wow
+    return largest if largest > 0 else None   
+lpf = prime_factors(numb)
+if prime_factors(numb):
+    print(f"The largest prime factor of {numb} is {lpf}")
+else:
+    print("No prime factors")
+            
