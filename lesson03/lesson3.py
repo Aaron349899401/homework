bf2b9ca3869b9b50d188e185314eb4229f5dcc43
unknown <aarongoogoo29@gmail.com>
2025-10-04 17:21:07 -0400

    completed

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,7 @@
 # Lesson 3
+import math
+num = int(input("Enter a number: "))
+side_length = math.floor(math.sqrt(num))
+largest_area = side_length * side_length
+print(f"The largest possible area for your input value is {largest_area}")
+print(f"The side length of this square is {side_length}")
