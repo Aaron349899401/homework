46ab51cd91a14c668b6b340fb94d6ea921540f11
unknown <aarongoogoo29@gmail.com>
2025-10-05 15:44:41 -0400

    completed

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,9 @@
 # Lesson 27
+import re
+string = input("Enter your string: ")
+
+find_string = re.findall(r"[a-zA-Z ]", string)
+result = "".join(find_string)
+better_result = re.sub(r"\s+", " ", result).strip()
+
+print(better_result)
