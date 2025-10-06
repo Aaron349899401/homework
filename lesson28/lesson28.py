f1588802bccabe1ed33395a49533e2a503ba831d
unknown <aarongoogoo29@gmail.com>
2025-10-06 12:56:08 -0400

    completed

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,7 @@
 # Lesson 28
+string = str(input("Enter your Palidrome: "))
+reversed_string = string[::-1]
+if string == reversed_string:
+    print("PALINDROME!")
+else:
+    print("Not a PALINDROME!")
