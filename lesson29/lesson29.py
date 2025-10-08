05160103508c67698fe255d4d89fff684fd05ce5
unknown <aarongoogoo29@gmail.com>
2025-10-07 20:17:56 -0400

    completed

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,13 @@
 # Lesson 29
+string = input("Enter your string: ")
+
+def cons_check(string):
+    consonants = set("bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ")
+    count = 0
+    for char in string:
+        if char in consonants:
+            count += 1
+    return count
+
+result = cons_check(string)
+print(result)
