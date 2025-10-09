4426b0b13f26ef52db1420043c3791efc55ea395
unknown <aarongoogoo29@gmail.com>
2025-10-09 10:47:22 -0400

    completed

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,12 @@
 # Lesson 30
+N = int(input("Enter you number: "))
+def pattern(N):
+    for i in range(1, N + 1):
+        monkey = ""
+        for poop in range(i):
+            if poop % 2 == 0:
+                monkey += "1"
+            else:
+                monkey += "0"
+        print(monkey)
+pattern(N)
