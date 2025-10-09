c062a7bae841bffb588b7b2ec3ab71a835ae9c63
unknown <aarongoogoo29@gmail.com>
2025-10-09 14:42:28 -0400

    completed

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,11 @@
 # Lesson 31
+def anagram_check(word1, word2):
+    return sorted(word1) == sorted(word2)
+
+word1 = input("Enter your word: ")
+word2 = input("Enter your second word bro: ")
+
+if anagram_check(word1, word2):
+    print("AMOGUS: your word is an anagram!")
+else:
+    print("Nah it's not!")
