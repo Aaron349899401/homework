22161d73c5b5c86a844adb2d970b1b56836b544d
unknown <aarongoogoo29@gmail.com>
2025-10-10 13:06:56 -0400

    completed

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,12 @@
 # Lesson 32
+string1 = input("Enter your first string: ")
+string2 = input("Enter your second string: ")
+sort1 = sorted(string1)
+sort2 = sorted(string2)
+common_char = []
+for i in sort1:
+    for wow in sort2:
+        if wow == i and sort1 not in common_char:
+            common_char.append(wow)
+
+print("Common Characters:", common_char)
