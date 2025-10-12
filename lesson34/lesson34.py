2f3c97447255954b5752fca3968d9256699ad7d9
unknown <aarongoogoo29@gmail.com>
2025-10-12 17:31:31 -0400

    completed

--- lesson34/lesson34.py
+++ lesson34/lesson34.py
@@ -1,1 +1,11 @@
 # Lesson 34
+from random import randrange
+
+def amogus(string):
+    return [int(x.strip()) for x in string.split(",")]
+    
+def random_int(start, end, frequency):
+    return [randrange(start, end + 1) for _ in range(frequency)]
+
+print(amogus("1, 2, 3, 6, 7"))
+print(random_int(1, 1000, 20))
