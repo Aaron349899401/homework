5aee4db6a0a11c41057cfdb81441edb9769eed2f
unknown <aarongoogoo29@gmail.com>
2025-10-12 17:43:36 -0400

    completed

--- lesson35/lesson35.py
+++ lesson35/lesson35.py
@@ -1,1 +1,8 @@
 # Lesson 35
+def remove_dupes(wow):
+    better_wow = set(wow)
+    return list(better_wow)
+whoa = input("Enter your list: ")
+wow = [stuff.strip() for stuff in whoa.split(",")]
+
+print(sorted(remove_dupes(wow)))
