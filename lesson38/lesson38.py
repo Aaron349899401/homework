ba870fb9c1b2c462b53f44da18745bf535a3049b
unknown <aarongoogoo29@gmail.com>
2025-10-15 20:49:38 -0400

    completed

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,9 @@
 # Lesson 38
+list = []
+for first in range(100, 1000):
+    for second in range(100, 1000):
+        num = str(first * second)
+        if num == num[::-1]:
+            list.append(int(num))
+print(max(list))
+            
