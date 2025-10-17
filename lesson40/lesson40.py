1c6269b904fbb11b45a7520f0c32f89a90333512
unknown <aarongoogoo29@gmail.com>
2025-10-17 12:01:10 -0400

    completed

--- lesson40/lesson40.py
+++ lesson40/lesson40.py
@@ -1,1 +1,13 @@
 # Lesson 40
+num = int(input("Enter a number: "))
+prime = []
+for i in range(2, num + 1):
+    poop = True
+    for wow in range(2, int(i**0.5) + 1):
+        if i % wow == 0:
+            poop = False
+            break
+    if poop:
+        prime.append(i)
+print(prime)
+        
