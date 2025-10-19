65e442d22d68a1c83072fbd90446448cc08d1c3f
unknown <aarongoogoo29@gmail.com>
2025-10-19 19:19:45 -0400

    completed

--- lesson42/lesson42.py
+++ lesson42/lesson42.py
@@ -1,1 +1,13 @@
 # Lesson 42
+def skibidi(toilet, rizz):
+    return [f"{chung} + {bun}" for i, chung in enumerate(toilet) for bun in toilet[i:] if chung + bun == rizz]
+
+poopoo = input("Enter your list of ascending integers: ")
+toilet = [int(x.strip()) for x in poopoo.split(",")]
+rizz = int(input("Enter your target value: "))
+
+result = skibidi(toilet, rizz)
+if skibidi(toilet, rizz):
+    print(f"{result} = {rizz}")
+else:
+    print("Nah")
