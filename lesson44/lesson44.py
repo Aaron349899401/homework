7d5c8f600cb584bce86e9f367d651931f815bf02
unknown <aarongoogoo29@gmail.com>
2025-10-20 10:14:09 -0400

    completed

--- lesson44/lesson44.py
+++ lesson44/lesson44.py
@@ -1,1 +1,13 @@
 # Lesson 44
+def dic(string):
+    dict = {}
+    for item in string:
+        item = item.strip()
+        dict[item] = dict.get(item, 0) + 1
+    return dict
+
+string = input("Enter your string: ")
+result = dic(string)
+# dict() prevent writing names as "dict"
+for address, value in result.items():
+    print(f"{address}:{value}")
