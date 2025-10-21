3bc47ae4f0a921732a061d35dbb39bc29083f66a
unknown <aarongoogoo29@gmail.com>
2025-10-21 12:01:41 -0400

    completed

--- lesson47/lesson47.py
+++ lesson47/lesson47.py
@@ -1,1 +1,11 @@
 # Lesson 47
+def among(us):
+    if us < 1:
+        return "Bruh"
+    result = 0
+    for num in range(1, us + 1):
+        result += num
+    return result
+
+us = int(input("Enter your positive integer: "))
+print(among(us))
